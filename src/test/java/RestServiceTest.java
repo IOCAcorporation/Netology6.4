2dd08885c65b02167aad1b014320717a91b7666d
IOCAcorporation <hri.st@yandex.com>
2025-09-07 12:21:22 +0300

    Add files via upload

---
+++ src/test/java/RestServiceTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class RestServiceTest {
+    @Test
+    public void shouldCalculateForRegistredAndOverLimit() {
+        RestService service = new RestService();
+        long expected = 10;
+        int actual = service.calculate(100_000, 13_000);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
+
