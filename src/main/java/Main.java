240bba7fd6b92ca7060f52e512215ee102146ea5
IOCAcorporation <hri.st@yandex.com>
2025-09-13 18:19:42 +0300

    Delete src directory

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        RestService count = new RestService();
-        int Month = count.calculate(120_000, 30_000);
-        System.out.println(Month);
-
-    }
-}
